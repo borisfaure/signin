6e2b0a5407712765140c5d2c425222de4a21c724
Boris Faure <boris@fau.re>
2017-04-09 23:39:22 +0200

    verify tokens (RS256-signature included)

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,19 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 extern crate base64;
+extern crate time;
+extern crate openssl;
 
 pub mod errors;
 
 use errors::Error;
+use std::fs::File;
+use time::Timespec;
+use openssl::bn::BigNum;
+use openssl::hash::MessageDigest;
+use openssl::pkey::PKey;
+use openssl::rsa::Rsa;
+use openssl::sign::Verifier;
 
 type JsonValue = serde_json::value::Value;
 type JsonObject = serde_json::map::Map<String, JsonValue>;
@@ -19,15 +28,56 @@ pub struct Header {
     pub kid: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug)]
 pub struct Payload {
     pub sub: String,
     pub iss: String,
     pub aud: String,
+    pub exp: Timespec,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Key {
+    pub kty: String,
+    pub alg: String,
+    #[serde(rename = "use")]
+    pub use_: String,
+    pub kid: String,
+    pub n: String,
+    pub e: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Keys {
+    pub keys: Vec<Key>,
+}
+
+
+#[derive(Debug)]
+pub struct Ctx {
+    pub client_id: String,
+    pub keys: Keys,
+}
+
+impl Ctx {
+    pub fn new(keys_path: String, client_id: String) -> Ctx {
+        let file = File::open(keys_path).unwrap();
+        let keys : Result<Keys, serde_json::Error> = serde_json::from_reader(&file);
+        let keys = keys.unwrap();
+
+        Ctx {
+            client_id: client_id,
+            keys: keys,
+        }
+    }
+}
+
+fn base64_decode_url(msg: &str) -> Result<Vec<u8>, base64::Base64Error> {
+    base64::decode_config(msg, base64::URL_SAFE)
 }
 
 fn decode_header(base64_hdr: &str) -> Result<Header, Error> {
-    let hdr = base64::decode(base64_hdr)?;
+    let hdr = base64_decode_url(base64_hdr)?;
     let hdr : Header = serde_json::from_slice(&hdr)?;
     Ok(hdr)
 }
@@ -44,8 +94,21 @@ fn json_get_str<'a>(obj: &'a JsonObject, name: &'static str) -> Result<&'a str, 
     }
 }
 
+fn json_get_numeric_date(obj: &JsonObject, name: &'static str) -> Result<Timespec, Error> {
+    let o : Option<&JsonValue> = obj.get(name);
+    if let Some(v) = o {
+        if !v.is_i64() {
+            return Err(Error::InvalidTypeField(name));
+        }
+        let sec = v.as_i64().unwrap();
+        return Ok(Timespec{sec: sec, nsec: 0});
+    } else {
+        return Err(Error::MissingField(name));
+    }
+}
+
 fn decode_payload(base64_payload: &str) -> Result<Payload, Error> {
-    let payload_json = base64::decode(base64_payload)?;
+    let payload_json = base64_decode_url(base64_payload)?;
     let obj : JsonValue = serde_json::from_slice(&payload_json)?;
     if !obj.is_object() {
         return Err(Error::InvalidTypeField(""));
@@ -55,6 +118,7 @@ fn decode_payload(base64_payload: &str) -> Result<Payload, Error> {
     let sub = json_get_str(obj, "sub")?;
     let iss = json_get_str(obj, "iss")?;
     let aud = json_get_str(obj, "aud")?;
+    let exp = json_get_numeric_date(obj, "exp")?;
 
 
     dbg!("obj:{:?}", obj);
@@ -66,53 +130,124 @@ fn decode_payload(base64_payload: &str) -> Result<Payload, Error> {
         sub: sub.to_string(),
         iss: iss.to_string(),
         aud: aud.to_string(),
+        exp: exp,
     })
 }
 
-pub fn google_signin_from_str(token: &str, secret: &[u8]) -> Result<String, Error> {
-    dbg!("token:{:?}", token);
-    let mut arr: Vec<&str> = token.split(".").collect();
-    dbg!("arr:{:?}", arr);
+fn verify_payload(ctx: &Ctx, payload: &Payload) -> Result<(), Error> {
+    if payload.aud != ctx.client_id {
+        return Err(Error::InvalidAudience);
+    }
+    if payload.iss != "accounts.google.com" && payload.iss != "https://accounts.google.com" {
+        return Err(Error::InvalidIssuer);
+    }
+    //let now = time::now_utc().to_timespec();
+    //if payload.exp.sec + 3600 < now.sec {
+    //    return Err(Error::Expired);
+    //}
+    Ok(())
+}
+
+fn find_key<'a>(ctx: &'a Ctx, hdr: &Header) -> Result<&'a Key, Error> {
+    for key in &ctx.keys.keys {
+        if key.kid == hdr.kid && key.alg == hdr.alg && key.use_ == "sig" {
+            return Ok(key);
+        }
+    }
+    Err(Error::NoMatchingSigningKey)
+}
+
+fn verify_rs256(txt: &str, key: &Key, sig: &[u8]) -> Result<(), Error> {
+
+    dbg!("n='{:?}'", key.n);
+    let n_decoded = base64_decode_url(&key.n)?;
+    let n = BigNum::from_slice(&n_decoded).unwrap();
+    dbg!("e='{:?}'", key.e);
+    let e_decoded = base64_decode_url(&key.e)?;
+    let e = BigNum::from_slice(&e_decoded).unwrap();
+
+    let rsa = Rsa::from_public_components(n, e).unwrap();
+    let key = PKey::from_rsa(rsa).unwrap();
+
+    let digest = MessageDigest::sha256();
+    let mut verifier = Verifier::new(digest, &key).unwrap();
+    verifier.update(txt.as_bytes()).unwrap();
+    let res = verifier.finish(sig);
+
+    match res {
+        Ok(true) => Ok(()),
+        Ok(false) => Err(Error::InvalidSignature),
+        Err(_) => Err(Error::InvalidSignature),
+    }
+}
+
+fn verify_signature(ctx: &Ctx, hdr: &Header, hdr_base64: &str,
+                    payload_base64: &str, sig: &[u8]) -> Result<(), Error> {
+    let txt = format!("{}.{}", hdr_base64, payload_base64);
+    dbg!("txt:{:?}", txt);
+
+    let key = find_key(ctx, hdr)?;
+    dbg!("key:{:?}", key);
+    dbg!("sig:{:?}", sig);
+
+    match key.alg.as_ref() {
+        "RS256" => {
+            verify_rs256(&txt, key, sig)
+        },
+        _ => {
+            Err(Error::UnsupportedAlgorithm)
+        }
+    }
+}
+
+pub fn google_signin_from_str(ctx: &Ctx, token: &str) -> Result<String, Error> {
+    let arr: Vec<&str> = token.split(".").collect();
     if arr.len() != 3 {
         return Err(Error::InvalidToken);
     }
 
-    let hdr = arr[0];
-    let payload = arr[1];
-    let sig  = arr[2];
+    let hdr_base64 = arr[0];
+    let payload_base64 = arr[1];
+    let sig_base64 = arr[2];
 
-    // Work on header
-    let hdr = decode_header(hdr)?;
-    let payload = decode_payload(payload)?;
+    let hdr = decode_header(hdr_base64)?;
+    let payload = decode_payload(payload_base64)?;
+    let sig = base64_decode_url(sig_base64)?;
+    let sig_slice : &[u8] = &sig;
 
+    verify_payload(&ctx, &payload)?;
+    verify_signature(&ctx, &hdr, &hdr_base64, &payload_base64, sig_slice)?;
 
     dbg!("hdr={:?}", hdr);
     dbg!("payload={:?}", payload);
 
-    Ok("42".to_string())
+    Ok(payload.sub)
 }
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     use std::io;
     use std::io::prelude::*;
-    use std::fs::File;
-    use super::*;
+
+    fn content_from_file(filename: &str) -> String {
+        let mut file = File::open(filename).unwrap();
+        let mut buf = String::new();
+        file.read_to_string(&mut buf);
+        buf.pop(); // remove trailing \n
+        buf
+    }
 
     #[test]
     fn from_token_file() {
-        let mut file = File::open("token").unwrap();
-        let mut token = String::new();
-        file.read_to_string(&mut token);
-        token.pop(); // remove trailing \n
+        let token = content_from_file("token");
+        let client_id = content_from_file("client_id");
+        let ctx = Ctx::new("google_keys.json".to_owned(), client_id);
 
-        file = File::open("secret").unwrap();
-        let mut secret = String::new();
-        file.read_to_string(&mut secret);
-        secret.pop(); // remove trailing \n
-
-        let res = google_signin_from_str(&token, &secret.as_bytes());
+        let res = google_signin_from_str(&ctx, &token);
         dbg!("res={:?}", res);
+        assert!(res.is_ok());
     }
 }
