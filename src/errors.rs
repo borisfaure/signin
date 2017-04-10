0bc34e37b732d61ba72a47f3462590bec92bae80
Boris Faure <boris@fau.re>
2017-04-10 23:20:33 +0200

    handle errors from openssl

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,13 @@
 use base64;
 use std::{error, fmt};
 use serde_json;
+use openssl;
 
 #[derive(Debug)]
 pub enum Error {
     DecodeBase64(base64::Base64Error),
     DecodeJson(serde_json::Error),
+    OpensslError(openssl::error::ErrorStack),
     InvalidToken,
     InvalidIssuer,
     InvalidAudience,
@@ -25,11 +27,13 @@ macro_rules! impl_from_error {
 }
 impl_from_error!(base64::Base64Error, Error::DecodeBase64);
 impl_from_error!(serde_json::Error, Error::DecodeJson);
+impl_from_error!(openssl::error::ErrorStack, Error::OpensslError);
 impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::DecodeBase64(ref err) => err.description(),
             Error::DecodeJson(ref err) => err.description(),
+            Error::OpensslError(ref err) => err.description(),
             Error::InvalidToken => "Invalid Token",
             Error::InvalidIssuer => "Invalid Issuer",
             Error::InvalidAudience => "Invalid Audience",
@@ -45,6 +49,7 @@ impl error::Error for Error {
         Some(match *self {
             Error::DecodeBase64(ref err) => err as &error::Error,
             Error::DecodeJson(ref err) => err as &error::Error,
+            Error::OpensslError(ref err) => err as &error::Error,
             ref e => e as &error::Error,
         })
     }
@@ -54,6 +59,7 @@ impl fmt::Display for Error {
         match *self {
             Error::DecodeBase64(ref err) => fmt::Display::fmt(err, f),
             Error::DecodeJson(ref err) => fmt::Display::fmt(err, f),
+            Error::OpensslError(ref err) => fmt::Display::fmt(err, f),
             Error::InvalidToken => write!(f, "{}", error::Error::description(self)),
             Error::InvalidIssuer => write!(f, "{}", error::Error::description(self)),
             Error::InvalidAudience => write!(f, "{}", error::Error::description(self)),
